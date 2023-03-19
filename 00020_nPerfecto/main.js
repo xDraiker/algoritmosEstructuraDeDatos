d94391673626b36510c0f9b0eadf30356099ba4e
Daniel <daniel.vz.gc97@gmail.com>
2023-03-19 02:42:38 -0600

    Numero perfecto sin arreglo

--- 00020_nPerfecto/main.js
+++ 00020_nPerfecto/main.js
@@ -2,18 +2,18 @@
 let number=496;
 
 /* 
-* Función para determinar si un número es perfecto
+* Funciones para determinar si un número es perfecto
 * No se toma en consideración number < 0 (Se debe añadir validación) 
 */
 function obtenerDivisores(n){
-    let divisores = [];
-    for(let i=0;i<=n/2;i++){
-        if(n%i==0) divisores.push(i);
+    let sumaDiv=0;
+    for(let i=1;i<=(n/2);i++){
+        if(n%i==0) sumaDiv+=i;
     }
-    return divisores;
+    return sumaDiv;
 }
 function nPerfecto(n){
-    let suma = obtenerDivisores(n).reduce((n1,n2) => n1+n2 );
+    let suma = obtenerDivisores(n);
     if (suma==n) return true;
     else return false;
 }
